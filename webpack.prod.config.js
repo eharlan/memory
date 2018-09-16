2a261154af8949a5a76131de9c7766a1a9f63803
Erik Harlan <harlanes14@gmail.com>
2018-09-16 15:17:18 -0600

    Adds basic webpack and babel setup

---
+++ webpack.prod.config.js
@@ -0,0 +1,29 @@
+
+var webpack = require("webpack");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractPlugin = new ExtractTextPlugin({
+    filename: './style.css'
+});
+module.exports = require('./webpack.config.js');
+delete module.exports.devtool;
+module.exports.plugins.push(
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+    })
+);
+module.exports.plugins.push(
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'common'
+    })
+);
+module.exports.plugins.push(extractPlugin);
+module.exports.module.rules.forEach(rule => {
+    delete rule.exclude;
+    return rule;
+});
+module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+        comments: false,
+        warnings: false
+    })
+);
