463bec8185584ce4d4b21f204ac37fcb5aa5d40f
Erik Harlan <harlanes14@gmail.com>
2019-02-16 22:23:05 +0000

    Adds css flip animation

--- src/components/card.js
+++ src/components/card.js
@@ -4,11 +4,16 @@ class Card extends React.Component {
 
 render() {
 
-return <div className="card">
-  <div className="card-container p-2">
-    <h4><b>{this.props.title}</b></h4>
-    <iframe src={this.props.details}  width="100%" height={this.props.height} frameBorder="0"></iframe>
-  </div></div>
+return <div class="flip-card">
+<div class="flip-card-inner">
+  <div class="flip-card-front">
+  <h4><b>{this.props.title}</b></h4>
+  </div>
+  <div class="flip-card-back">
+  <iframe src={this.props.details}  width="100%" height={this.props.height} frameBorder="0"></iframe>
+  </div>
+</div>
+</div>
 
 }
 
